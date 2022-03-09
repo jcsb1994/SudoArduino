ac50454edf5f0af3ad12621148d174becfbdaca6
Jean-Christophe Sicotte-Brisson <jeanchristophesicottebrisson@gmail.com>
2022-03-09 11:16:35 -0500

    Added timeout example

--- examples/timeout.cpp
+++ examples/timeout.cpp
@@ -2,13 +2,19 @@
 #include "SudoArduino.h"
 
 #define TIMEOUT_PERIOD_MS (7000)  // period occurs after 7 seconds
+unsigned long startTime;
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Hello");
+    startTime = (MAX_ULONG_VALUE - 1000); // simulates a start right before millis() overflow
+    Serial.println("Timer starts here");
 }
 
 void loop()
 {
-    if(isTimeOut())
+  delay(200);
+  if(isTimeOut(startTime, TIMEOUT_PERIOD_MS)) {
+    Serial.println("Timeout reached!");
+    while(69);
+  }
 }
